668a9c1f6db650267ed3ca466342d7d956527ef5
ArtemEvgTitov <>
2022-09-12 20:07:41 +0300

    Добавлено решение второй задачи

--- Seminar_3/task2.py
+++ Seminar_3/task2.py
@@ -5,3 +5,19 @@
 # список: ["йцу", "фыв", "ячс", "цук", "йцукен"], ищем: "йцу", ответ: -1
 # список: ["123", "234", 123, "567"], ищем: "123", ответ: -1
 # список: [], ищем: "123", ответ: -1
+
+st = "qwe"
+task_list = ["qwe", "asd", "zxc", "qwe", "ertqwe", "qwe"]
+ind = 0
+sh = 0
+m_ind = 0
+for i in task_list:
+    if i == st and sh < 2:
+        sh += 1
+        m_ind = ind
+    ind += 1
+
+if sh != 0:
+    print(f"{task_list} - ищем: {st}, ответ {m_ind} индексе")
+else:
+    print('-1')
