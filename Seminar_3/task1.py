732b2a900366d05b81fc72151d59abe6086d961b
ArtemEvgTitov <>
2022-09-12 19:53:16 +0300

    Добавлено решение первой задачи

--- Seminar_3/task1.py
+++ Seminar_3/task1.py
@@ -1,2 +1,23 @@
 # Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
 # ['gfh5', 'gfh2', '67', 'jy32', '3put'] - ищем 32 - находим по индексу 3
+
+
+task_list = ['gfh5', 'gfh2', '67', 'jy32', '3put']
+
+
+def task_list_2(list):
+    result = ''
+    for i in list:
+        if ord(i) > ord('/') and ord(i) < ord(':'):
+            result += i
+
+    return result
+
+
+number = input('Input number: ')
+ind = 0
+for i in task_list:
+    t_el = task_list_2(i)
+    if number == t_el:
+        print(f"{task_list} - ищем {number} - найдено на {ind} индексе")
+    ind += 1
