bbd8e14b7d4a89547378282460d09d54cf09fb5a
ArtemEvgTitov <>
2022-09-18 14:10:19 +0300

    Добавлено решение второй задачи

--- Seminar_4/task1.py
+++ Seminar_4/task1.py
@@ -9,3 +9,13 @@ print(list_of_str)
 for i in list_of_str:
     list_of_num.append(int(i))
 print(f'Наибольшее число равно {min(list_of_num)}, а наибольшее {max(list_of_num)}')
+
+# второе решение
+my_str=(1,3,4,5,6)
+print(*my_str, sep=" ")
+
+max_value=max(my_str, key=lambda i: int(i))
+min_value=min(my_str, key=lambda i: int(i))
+
+print(max_value)
+print(min_value)
